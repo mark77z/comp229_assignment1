d48321fd15c02dba8ad794ef5ef0a1a0f79e45e8
Marco Mejia <markmejiar@gmail.com>
2021-02-10 01:57:25 -0500

    First commit for assignment 1

---
+++ routes/index.js
@@ -0,0 +1,48 @@
+var express = require('express');
+var router = express.Router();
+
+let contactName = 'kkkk';
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { 
+    title: 'Home',
+    contactName: contactName
+  });
+});
+
+/* GET home page. */
+router.get('/home', function(req, res, next) {
+  res.render('index', { 
+    title: 'Home',
+    contactName: contactName
+  });
+});
+
+/* GET About Us page. */
+router.get('/about', function(req, res, next) {
+  res.render('index', { title: 'About'});
+});
+
+/* GET Products page. */
+router.get('/products', function(req, res, next) {
+  res.render('index', { title: 'Products'});
+});
+
+/* GET Services page. */
+router.get('/services', function(req, res, next) {
+  res.render('index', { title: 'Services'});
+});
+
+/* GET Contact Us page. */
+router.get('/contact', function(req, res, next) {
+  res.render('index', { title: 'Contact'});
+});
+
+/* POST Contact Us page. */
+router.post('/contact', function(req, res, next) {
+  contactName = req.body.fname;
+  res.redirect('/home');
+});
+
+module.exports = router;
