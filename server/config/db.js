67c96ba06beddfd5a304da39068b48e612c6ae60
Marco Mejia <markmejiar@gmail.com>
2021-02-25 16:06:03 -0500

    Changed db connection URI to MongoDB Atlas

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,5 @@
 module.exports = 
 {
-    "URI" : "mongodb://localhost/book_store"
+    //"URI" : "mongodb://localhost/book_store"
+    "URI" : "mongodb+srv://markmejiar:ZEsmyOMqxJeHJHMT@mongodbserver.t1hom.mongodb.net/book_store?retryWrites=true&w=majority"
 }
